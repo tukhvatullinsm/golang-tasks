e5c25d1b804febd593a09c4647e80e69c74f4899
Salavat <salavat.tukhvatullin1@gmail.com>
2026-01-10 11:05:55 +0300

    Task#3 Ready

--- sprint-1/hotelbusiness/hotels.go
+++ sprint-1/hotelbusiness/hotels.go
@@ -2,6 +2,10 @@
 
 package hotelbusiness
 
+import (
+	"sort"
+)
+
 type Guest struct {
 	CheckInDate  int
 	CheckOutDate int
@@ -13,5 +17,29 @@ type Load struct {
 }
 
 func ComputeLoad(guests []Guest) []Load {
-	return []Load{}
+	calendar := make(map[int]int, len(guests))
+	dates := make([]int, 0)
+	result := make([]Load, 0, len(guests))
+	lastDayGuest := int(0)
+	for _, guest := range guests {
+		for i := guest.CheckInDate; i < guest.CheckOutDate; i++ {
+			calendar[i]++
+		}
+		_, ok := calendar[guest.CheckOutDate]
+		if !ok {
+			calendar[guest.CheckOutDate] = 0
+		}
+	}
+	for day, _ := range calendar {
+		dates = append(dates, day)
+	}
+	sort.Ints(dates)
+
+	for _, value := range dates {
+		if calendar[value] != lastDayGuest {
+			result = append(result, Load{StartDate: value, GuestCount: calendar[value]})
+		}
+		lastDayGuest = calendar[value]
+	}
+	return result
 }
